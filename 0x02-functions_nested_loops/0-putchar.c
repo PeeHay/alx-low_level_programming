2e27966cad6370b3ed2f6a1fc97e9e3844235bcf
PeeHay <preatie911.com>
2022-10-18 17:07:30 -0700

    once

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
 #include "main.h"
 /**
- * main - Program Entry point.
- * Description: This program prints "_putchar" without
- * the standard library functions printf, puts etc...
- * Return: Always 0 Success.
+ * Description: main - Print out a string to stdout.
+ * Return: 0 if successfull.
  */
-int main(void)
+int main(void);
 {
-	char string[] = "_putchar";
-	int i;
+	char putchar[] = "_putchar";
+	int i = 0;
 
-	i = 0;
-	while (string[i] != '\0')
+	while (putchar[i] != '\0')
 	{
-		_putchar(string[i]);
+		_putchar(putchar[i]);
 		i++;
 	}
 	_putchar('\n');
+
 	return (0);
 }
